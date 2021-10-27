306bf21bdf30907c90ab870e4784c9fe5f0d801a
Kibercroll <30548104+Kibercroll@users.noreply.github.com>
2021-10-27 21:43:18 +0400

    Add files via upload

---
+++ QuickSort.cs
@@ -0,0 +1,35 @@
+﻿namespace Chapter1
+{
+    public class QuickSort
+    {
+        private static int[] quickSort(int[] mass, int left, int right)
+        {
+            int swap, i = left, j = right, middle = mass[(left + right) / 2];
+            while (i <= j)
+            {
+                while (mass[i] < middle) i++;
+                while (mass[j] > middle) j--;
+
+                if (i <= j)
+                {
+                    if (mass[i] > mass[j])
+                    {
+                        swap = mass[i];
+                        mass[i] = mass[j];
+                        mass[j] = swap;
+                    }
+                    i++; j--;
+                }
+            }
+
+            if (left < j) quickSort(mass, left, j);
+            if (i < right) quickSort(mass, i, right);
+            return mass;
+        }
+
+        public static int[] Sort(int[] mass)
+        {
+            return quickSort(mass, 0, mass.Length - 1);
+        }
+    }
+}
