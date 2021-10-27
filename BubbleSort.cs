62ea8833252ca915b6db5ad78954124177a24649
Kibercroll <30548104+Kibercroll@users.noreply.github.com>
2021-10-27 21:43:36 +0400

    Add files via upload

---
+++ BubbleSort.cs
@@ -0,0 +1,23 @@
+﻿namespace Chapter1
+{
+    public class BubbleSort
+    {
+        public static int[] Sort(int[] mass)
+        {
+            int middle;
+            for (int i = 0; i < mass.Length; i++)
+            {
+                for (int x = 0; x < mass.Length - 1; x++)
+                {
+                    if (mass[x] > mass[x + 1])
+                    {
+                        middle = mass[x];
+                        mass[x] = mass[x + 1];
+                        mass[x + 1] = middle;
+                    }
+                }
+            }
+            return mass;
+        }
+    }
+}
